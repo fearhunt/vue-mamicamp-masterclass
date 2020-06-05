0723f3774bd504fad9557820561e6e288b7c56cd
fearhunt <firhanmahdi@gmail.com>
2020-06-05 10:22:31 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to forum!</h1>
+    <div v-for="thread in threads">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+console.log(sourceData)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
