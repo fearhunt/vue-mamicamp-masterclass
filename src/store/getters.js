d6a1dc2c0044af4ce8e89946ae6c3c27a6239bcc
fearhunt <firhanmahdi@gmail.com>
2020-06-06 14:39:24 +0700

    fetch user

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,7 +2,7 @@ import {countObjectProperties} from '@/utils'
 
 export default {
   authUser (state) {
-    return {}
+    return state.users[state.authId]
   },
 
   userThreadsCount: state => id => countObjectProperties(state.users[id].threads),
