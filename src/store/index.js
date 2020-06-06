1ff96910744b6f86c62f94ad1c60daa446477f2b
fearhunt <firhanmahdi@gmail.com>
2020-06-06 09:04:12 +0700

    fetch firebase with vuex action

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import {countObjectProperties} from '@/utils'
@@ -101,6 +102,39 @@ export default new Vuex.Store({
 
     updateUser ({commit}, user) {
       commit('setUser', {userId: user['.key'], user})
+    },
+
+    fetchThread ({state, commit}, {id}) {
+      console.log('[phew!] doc', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('threads').child(id).once('value', snapshot => {
+          const thread = snapshot.val()
+          commit('setThread', {threadId: snapshot.key, thread: {...thread, '.key': snapshot.key}})
+          resolve(state.threads[id])
+        })
+      })
+    },
+
+    fetchUser ({state, commit}, {id}) {
+      console.log('[phew!] user‍', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('users').child(id).once('value', snapshot => {
+          const user = snapshot.val()
+          commit('setUser', {userId: snapshot.key, user: {...user, '.key': snapshot.key}})
+          resolve(state.users[id])
+        })
+      })
+    },
+
+    fetchPost ({state, commit}, {id}) {
+      console.log('[phew!] post‍', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('posts').child(id).once('value', snapshot => {
+          const post = snapshot.val()
+          commit('setPost', {postId: snapshot.key, post: {...post, '.key': snapshot.key}})
+          resolve(state.posts[id])
+        })
+      })
     }
   },
 
